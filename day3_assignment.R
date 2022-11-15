d763009789923fb6f4be17401cd0554a36c60aa8
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-11-15 20:37:20 +0100

    Update day3_assignment.R

--- day3_assignment.R
+++ day3_assignment.R
@@ -8,8 +8,7 @@
 # 1 - load GGIR
 library(GGIR)
 
-# 2 - Run GGIR with automated sleep detection
-
+# 2 - Try out the arguments you are more interested in.
 GGIR(datadir = "C:/mystudy/mydata",
      outputdir = "D:/myresults",
      # fill the arguments here
