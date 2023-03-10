072e73d41818b338f6cd980b7e564b1ff7de4724
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-03-10 12:51:20 +0100

    assignment 1 for GGIR standard training

---
+++ assignments/GGIRstandard_assignment1.R
@@ -0,0 +1,11 @@
+# Download file: https://bit.ly/3OGtwsq 
+
+# Open RStudio and an empty script
+# Create a GGIR function call
+# Define datadir and outputdir
+# Define mode to run GGIR parts 1 and 2
+# Make sure you only analyse data from the first midnight onwards
+# We are only interested in teh analysis of the first 3 days
+# Run the script via the source button
+# Advanced: look up the output and meaning of variables (vignette)
+# Optional: try to run GGIR parts 1 and 2 on your own data
