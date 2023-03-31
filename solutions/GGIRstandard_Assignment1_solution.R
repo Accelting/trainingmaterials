604cf206f755dc69dc78b3659d466f6292c33d30
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-03-31 09:17:43 +0200

    solution to standard assignment 1

---
+++ solutions/GGIRstandard_Assignment1_solution.R
@@ -0,0 +1,25 @@
+# Download file: https://bit.ly/3OGtwsq 
+
+# Open RStudio and an empty script
+# Create a GGIR function call
+# Define datadir and outputdir
+# Define mode to run GGIR parts 1 and 2
+# Make sure you only analyse data from the first midnight onwards
+# We are only interested in the analysis of the first 3 days
+# Run the script via the source button
+# Advanced: look up the output and meaning of variables (vignette)
+# Optional: try to run GGIR parts 1 and 2 on your own data
+
+
+# 1 - load GGIR
+library(GGIR)
+
+# 2 - GGIR function call
+
+GGIR(# assignment 1
+     datadir = "C:/GGIRtraining/file/",
+     outputdir = "C:/GGIRtraining/assignments/",
+     mode = 1:2,
+     strategy = 4,
+     maxdur = 3,
+     max_calendar_days = 3)
