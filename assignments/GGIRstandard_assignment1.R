7933714e1907b342c857e249ea7388faf41095d3
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-03-31 09:07:17 +0200

    typo in GGIRstandard_assignment1

--- assignments/GGIRstandard_assignment1.R
+++ assignments/GGIRstandard_assignment1.R
@@ -5,7 +5,7 @@
 # Define datadir and outputdir
 # Define mode to run GGIR parts 1 and 2
 # Make sure you only analyse data from the first midnight onwards
-# We are only interested in teh analysis of the first 3 days
+# We are only interested in the analysis of the first 3 days
 # Run the script via the source button
 # Advanced: look up the output and meaning of variables (vignette)
 # Optional: try to run GGIR parts 1 and 2 on your own data
