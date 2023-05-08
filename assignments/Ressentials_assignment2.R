bfe73def9823981222e96f5b0a5c5a3fcdf13219
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-05-08 12:55:10 +0200

    rename assignments to include a new assignment 1

--- assignments/Ressentials_assignment2.R
+++ assignments/Ressentials_assignment2.R
@@ -1,7 +1,11 @@
-# Download file: https://bit.ly/3OGtwsq 
+# Download this file and store it in a known location in your machine: 
+# https://bit.ly/3OGtwsq 
 
 # Open RStudio and an empty script
-# Create a GGIR function call
-# Define datadir and outputdir 
-# Tip: look up the documentation of these parameters
-# Run the script via the source button
+
+# Set the path to the directory where your file is located 
+# Save the directory path in an object named rawAccDir
+
+# Make sure the defined directory exists in your computer
+# If it does not exist, ammend the directory path stored in your object
+
--- assignments/Ressentials_assignment3.R
+++ assignments/Ressentials_assignment3.R
@@ -1,9 +1,7 @@
 # Download file: https://bit.ly/3OGtwsq 
 
-# Run GGIR parts 1 and 2
-# set up two thresholds for MVPA (100 and 140)
-
-# tip: look up the documentation to find the parameter that allows to 
-# control the mvpa threshold
-
-# tip2: it is in params_phyact
+# Open RStudio and an empty script
+# Create a GGIR function call
+# Define datadir and outputdir 
+# Tip: look up the documentation of these parameters
+# Run the script via the source button
