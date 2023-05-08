b30b26f9f54256c77637566f5691540bf7fff717
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-05-08 10:32:24 +0200

    R essentials contents script

---
+++ Ressentials.R
@@ -0,0 +1,19 @@
+# install R & RStudio (updates)
+# The RStudio interface
+# R scripts
+# Source and run buttons
+# Objects
+# Classes
+# Vectors
+# Setting directories and files paths
+# R functions & documentation
+# Specifying paths to folders and files
+# Install and update R packages
+# Install GGIR
+# Check package version
+# The GGIR function (quick presentation)
+# Run GGIR for first time (datadir, outputdir)
+# Example of a character argument in GGIR
+# Example of a numeric argument in GGIR
+# Example of a logical argument in GGIR
+# Example of using vectors in GGIR
