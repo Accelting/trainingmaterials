e6a6ebd573cb0f9f8f71516952d28d5348b017a4
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-09-07 12:58:43 +0200

    include notes for running assignments in R essentials course

--- Ressentials.R
+++ Ressentials.R
@@ -8,14 +8,18 @@
 # logical
 # Vectors
 # Subsetting
+# Assignment 1
 # Setting directories and files paths
+# Assignment 2
 # R functions & documentation
 # Install and update R packages
 # Install GGIR
 # Check package version
 # The GGIR function (quick presentation)
+# Assignment 3
 # Run GGIR for first time (datadir, outputdir)
 # Example of a numeric argument in GGIR
 # Example of a character argument in GGIR
 # Example of a logical argument in GGIR
 # Example of using vectors in GGIR
+# Assignment 4
