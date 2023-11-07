6d28f3763d37d004585618e86250a941b2065207
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-11-07 08:06:31 +0100

    revise names of assignments in day 2 Ressetials

--- Ressentials_day2.R
+++ Ressentials_day2.R
@@ -3,10 +3,8 @@
 # Install GGIR
 # Check package version
 # The GGIR function (quick presentation)
-# Assignment 3
-# Run GGIR for first time (datadir, outputdir)
-# Example of a numeric argument in GGIR
-# Example of a character argument in GGIR
-# Example of a logical argument in GGIR
-# Example of using vectors in GGIR
-# Assignment 4
+# Assignment 3: Run GGIR for first time (datadir, outputdir)
+# Assignment 4: Example of a numeric argument in GGIR
+# Assignment 5: Example of a character argument in GGIR
+# Assignment 6: Example of a logical argument in GGIR
+# Assignment 7: Example of using vectors in GGIR
